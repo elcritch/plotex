118ee535945f865f00cb6557f56664377d6c1c08
Jaremy Creechley <creechley@gmail.com>
2019-06-22 16:39:36 -0600

    add info

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Plotex.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
