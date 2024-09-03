a3b1a3e3d5fbd692b7e6d13bd3afd63167ddb9b2
Jaremy Creechley <creechley@gmail.com>
2024-09-02 20:52:25 -0600

    css tweak

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Plotex.MixProject do
   def project do
     [
       app: :plotex,
-      version: "0.5.4",
+      version: "0.5.5",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
